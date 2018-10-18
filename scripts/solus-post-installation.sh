85b0b11f8b15783fa72e583a6940971433808613
Marcus Sneitz <marcus.sneitz@gmail.com>
2018-10-18 20:47:07 +0200

    updated flags & software, added reboot

--- scripts/solus-post-installation.sh
+++ scripts/solus-post-installation.sh
@@ -5,45 +5,54 @@ echo "Starting post-install setup..."
 
 # Updating system
 echo "Updating system..."
-sudo eopkg up
+sudo eopkg -y up
 echo "System update done!"
 
 # System software
 echo "Installing system software..."
-sudo eopkg it neofetch
-sudo eopkg it lm_sensors
-sudo eopkg it inxi
+sudo eopkg it -y neofetch
+sudo eopkg it -y lm_sensors
+sudo eopkg it -y inxi
 
 # Software
 echo "Installing software..."
-sudo eopkg it discord
-sudo eopkg it spotify
-sudo eopkg it albert
-sudo eopkg it gnome-tweaks
-sudo eopkg it steam
-sudo eopkg it google-chrome-stable
-sudo eopkg it mscorefonts
-sudo eopkg it lutris
-sudo eopkg it obs-studio
+sudo eopkg it -y discord
+sudo eopkg it -y gnome-tweaks
+sudo eopkg it -y steam
+sudo eopkg it -y mscorefonts
+sudo eopkg it -y lutris
+sudo eopkg it -y obs-studio
+
+# 3rd party software
+echo "Installing Google Chrome..."
+sudo eopkg bi -y --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml
+sudo eopkg it -y google-chrome-*.eopkg
+sudo rm google-chrome-*.eopkg
+
+echo "Installing Spotify..."
+sudo eopkg bi -y --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/music/spotify/pspec.xml
+sudo eopkg it -y spotify*.eopkg
+sudo rm spotify*.eopkg
 
 # Development
 echo "Installing development related software..."
-sudo eopkg it -c system.devel
-sudo eopkg it git
-sudo eopkg it vscode
-sudo eopkg it nodejs
-sudo eopkg it etcher
+sudo eopkg it -y -c system.devel
+sudo eopkg it -y git
+sudo eopkg it -y vscode
+sudo eopkg it -y nodejs
+sudo eopkg it -y etcher
 
 # Drivers
 echo "Installing additional drivers and utility..."
-sudo eopkg it razer-drivers
-sudo eopkg it polychromatic
+sudo eopkg it -y razer-drivers
+sudo eopkg it -y polychromatic
+sudo eopkg it -y piper
 
 # Themes & Icons
 echo "Installing themes & icons..."
-sudo eopkg it arc-gtk-theme
+sudo eopkg it -y arc-gtk-theme
 gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
-sudo eopkg it papirus-icon-theme
+sudo eopkg it -y papirus-icon-theme
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
 
 # Set chrome as default browser
@@ -54,9 +63,30 @@ xdg-settings set default-web-browser google-chrome.desktop
 echo "Disabling mouse acceleration..."
 gsettings set org.gnome.desktop.peripherals.mouse accel-profile flat
 
+# Disabling screensaver
+echo "Disabling screensaver..."
+gsettings set org.gnome.desktop.session idle-delay 0
+
+# Disabling workspaces
+echo "Disabling workspaces..."
+gsettings set org.gnome.shell.overrides dynamic-workspaces false
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 1
+
 # Update boot manager
 echo "Updating boot manager..."
 sudo clr-boot-manager set-timeout 0
 sudo clr-boot-manager update
 
+echo
 echo "Done!"
+echo
+
+echo "A reboot is recommended at this point."
+read -p "Do you want to reboot? (yes/no)" -r
+echo
+if [[ $REPLY =~ ^[Yy][Ee][Ss]$ ]]
+then
+  systemctl reboot
+else
+	echo "You choose not to reboot, things may not work as expected until you do."
+fi
